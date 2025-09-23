b1f777b7fde7ead013182ba86fe56fb56f4b1467
Athira Anil <athiraanil351@gmail.com>
2025-09-23 21:14:56 +0530

    wip

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,9 @@ const corsOptions = {
   allowedHeaders: ["Content-Type", "Authorization"]
 };
 app.use(cors(corsOptions));
+
+app.options("*", cors(corsOptions));
+
 app.use(express.json());
 
 // Routes
