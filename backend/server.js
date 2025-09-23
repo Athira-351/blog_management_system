00ddf296d4f26f6c9a618679cdb61c88d1a0a37d
Athira Anil <athiraanil351@gmail.com>
2025-09-23 19:26:17 +0530

    wip

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(cors({
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
-app.options("*", cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
 // Routes
