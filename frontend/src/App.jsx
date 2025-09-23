8ba9dc503ec0e8cd68763622c150fc551909c823
Athira Anil <athiraanil351@gmail.com>
2025-09-23 20:02:46 +0530

    route defined for /

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         <Navbar />
 
         <Routes>
-          {/* <Route path="/" element={<Navigate to="/login" />} /> */}
+          <Route path="/" element={<Navigate to="/login" />} />
 
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
